6506f08eb6a5d40e0cf84b3314a1f9657ccb043f
LuFerdioly <lucianaferdioly@gmail.com>
2023-10-21 22:28:49 -0300

    add project

---
+++ LeagueDotaLuciana/LeagueDotaLuciana/LeagueDotaLucianaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeagueDotaLucianaApp.swift
+//  LeagueDotaLuciana
+//
+//  Created by Luciana Fernandes de Oliveira on 21/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LeagueDotaLucianaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
